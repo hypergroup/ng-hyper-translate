c9acec1aa2e5b2a97c46b2010163de21cbe32de8
CamShaft <bytheway.cameron@gmail.com>
2014-01-15 16:44:52 -0700

    support translation of attributes

--- index.js
+++ index.js
@@ -11,53 +11,70 @@ var Polyglot = require('polyglot');
 
 var pkg = module.exports = angular.module('ng-hyper-translate', ['ng-hyper']);
 
+pkg.factory('hyperTranslate', [
+  'hyper',
+  function(hyper) {
+    return function(path, params, $scope, fn) {
+      var polyglot = new Polyglot();
+
+      var name;
+      hyper.get(path, $scope, function(value, req) {
+        if (!value) return;
+        name = '$$' + req.target;
+        var phrases = {};
+        phrases[name] = value;
+
+        polyglot.extend(phrases);
+        translate();
+      });
+
+      if (!params) return;
+      angular.forEach(params.split(','), function(expr) {
+        var parts = expr.trim().split(' as ');
+        var path = parts[0];
+        var target = parts[1];
+
+        hyper.get(path, $scope, function(value, req) {
+          var t = target || req.target;
+          $scope[t] = value;
+          translate();
+        });
+      });
+
+      function translate() {
+        if (!name) return;
+        var res = polyglot.t(name, $scope);
+        if (res === name) return fn('');
+        fn(res);
+      }
+    };
+  }
+]);
+
 /**
  * Initialize the hyperTranslate directive
  */
 
 pkg.directive('hyperTranslate', [
-  'hyper',
-  function(hyper) {
+  'hyperTranslate',
+  function(translate) {
     return {
-      scope: true,
       restrict: 'A',
       link: function($scope, elem, attrs) {
-        var polyglot = new Polyglot();
+        var attrParts = attrs.hyperTranslate.split('->');
 
-        var transParts = attrs.hyperTranslate.split('<-');
-        var key = transParts[0].trim();
-        var params = transParts[1];
+        if (attrParts.length === 1) attrParts.unshift('text');
+        var attr = attrParts[0].trim();
+        var translations = attrParts[1].trim();
 
-        var name;
-        hyper.get(key, $scope, function(value, req) {
-          if (!value) return;
-          name = '$$' + req.target;
-          var phrases = {};
-          phrases[name] = value;
-
-          polyglot.extend(phrases);
-          translate();
+        var parts = translations.split('<-');
+        var path = parts[0].trim();
+        var params = parts[1] || '';
+        translate(path, params, $scope, function(value) {
+          attr === 'text'
+            ? elem.text(value)
+            : elem.attr(attr, value);
         });
-
-        if (!params) return;
-        angular.forEach(params.split(','), function(expr) {
-          var parts = expr.trim().split(' as ');
-          var path = parts[0];
-          var target = parts[1];
-
-          hyper.get(path, $scope, function(value, req) {
-            var t = target || req.target;
-            $scope[t] = value;
-            translate();
-          });
-        });
-
-        function translate() {
-          if (!name) return;
-          var res = polyglot.t(name, $scope);
-          if (res === name) return elem.text('');
-          elem.text(res);
-        }
       }
     };
   }

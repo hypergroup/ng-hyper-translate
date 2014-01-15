1db49a737ca293fff0270fc58abaa7548576c4ec
CamShaft <bytheway.cameron@gmail.com>
2014-01-14 22:50:46 -0700

    export package name

--- ng-hyper-translate.js
+++ ng-hyper-translate.js
@@ -445,63 +445,71 @@ require.register("ng-hyper-translate/index.js", function(exports, require, modul
  * Module dependencies
  */
 
+var angular = window.angular;
 var Polyglot = require('polyglot');
 
 /**
  * Initialize the ng-hyper-translate module
  */
 
-angular.module('ng-hyper-translate', ['ng-hyper'])
-  .directive('ngHyperTranslate', [
-    'hyper',
-    'hyperStatus',
-    function(hyper, status) {
-      return {
-        scope: true,
-        restrict: 'A',
-        link: function($scope, elem, attrs) {
-          status.loading(elem);
-          var polyglot = new Polyglot();
+var pkg = module.exports = angular.module('ng-hyper-translate', ['ng-hyper']);
 
-          var transParts = attrs.hyperTranslate.split('<-');
-          var key = transParts[0].trim();
-          var params = transParts[1];
+/**
+ * Initialize the hyperTranslate directive
+ */
 
-          var name;
-          hyper.get(key, $scope, function(value, req) {
-            if (!value) return;
-            name = '$$' + req.target;
-            var phrases = {};
-            phrases[name] = value;
+pkg.directive('hyperTranslate', [
+  'hyper',
+  'hyperStatus',
+  function(hyper, status) {
+    return {
+      scope: true,
+      restrict: 'A',
+      link: function($scope, elem, attrs) {
+        status.loading(elem);
+        var polyglot = new Polyglot();
 
-            polyglot.extend(phrases);
+        var transParts = attrs.hyperTranslate.split('<-');
+        var key = transParts[0].trim();
+        var params = transParts[1];
+
+        var name;
+        hyper.get(key, $scope, function(value, req) {
+          if (!value) return;
+          name = '$$' + req.target;
+          var phrases = {};
+          phrases[name] = value;
+
+          polyglot.extend(phrases);
+          translate();
+        });
+
+        if (!params) return;
+        angular.forEach(params.split(','), function(expr) {
+          var parts = expr.trim().split(' as ');
+          var path = parts[0];
+          var target = parts[1];
+
+          hyper.get(path, $scope, function(value, req) {
+            var t = target || req.target;
+            $scope[t] = value;
             translate();
           });
+        });
 
-          if (!params) return;
-          angular.forEach(params.split(','), function(expr) {
-            var parts = expr.trim().split(' as ');
-            var path = parts[0];
-            var target = parts[1];
-
-            hyper.get(path, $scope, function(value, req) {
-              var t = target || req.target;
-              $scope[t] = value;
-              translate();
-            });
-          });
-
-          function translate() {
-            if (!name) return status.loading(elem);
-            var res = polyglot.t(name, $scope);
-            if (res === name) return elem.text('');
-            elem.res(res);
-            status.loaded(elem);
-          }
+        function translate() {
+          if (!name) return status.loading(elem);
+          var res = polyglot.t(name, $scope);
+          if (res === name) return elem.text('');
+          elem.res(res);
+          status.loaded(elem);
         }
-      };
-    }
-  ]);
+      }
+    };
+  }
+]);
+
+pkg.name = 'ng-hyper-translate';
 
 });
 require.alias("airbnb-Polyglot.js/lib/polyglot.js", "ng-hyper-translate/deps/polyglot/lib/polyglot.js");
